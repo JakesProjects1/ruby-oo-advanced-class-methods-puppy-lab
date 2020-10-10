582197a1ec338876acf32d4daa7d88509f7f08d1
JakesProjects1 <jacob@kagon.net>
2020-10-10 15:30:25 -0400

    finished lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    attr_reader :name
+    # attr_accessor :clear_all
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+     def self.print_all
+        @@all.each do |dog|
+           puts dog.name
+        end
+    end
+
+      def save 
+       @@all << self
+     end
+
+
+
+end
