41f3dee0e6c1f9e8ffd6d7c17fc692113cfeb03c
JakesProjects1 <jacob@kagon.net>
2020-10-11 14:31:32 -0400

    new changes

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,34 +2,92 @@
 require 'pry'
 class Dog
 
-    attr_reader :name
-    # attr_accessor :clear_all
+  attr_reader :name
+  
+@@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+  #binding.pry 
     
-    @@all = []
 
-    def initialize(name)
-        @name = name
-        self.save
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |dog_name|
+      puts dog_name.name
     end
+  end
 
-    def self.all
-      @@all
-    end
+  def save
+    @@all << self
+  end
 
-    def self.clear_all
-        @@all.clear
-    end
-
-     def self.print_all
-        @@all.each do |dog|
-           puts dog.name
-        end
-    end
-
-      def save 
-       @@all << self
-     end
 
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+
+#     attr_reader :name
+#     # attr_accessor :clear_all
+    
+#     @@all = []
+
+#     def initialize(name)
+#         @name = name
+#         self.save
+#     end
+
+#     def self.all
+#       @@all
+#     end
+
+#     def self.clear_all
+#         @@all.clear
+#     end
+
+#      def self.print_all
+#         @@all.each do |dog|
+#            puts dog.name
+#         end
+#     end
+
+#       def save 
+#        @@all << self
+#      end
+
+
+
+# end
